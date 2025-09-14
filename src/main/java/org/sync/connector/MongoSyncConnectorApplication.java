76b3db6eff093688d895431d9f8691a33100ce93
jaeyounglee <ljyda214@gmail.com>
2025-09-14 13:32:44 +0900

    git init

---
+++ src/main/java/org/sync/connector/MongoSyncConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.sync.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSyncConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSyncConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sync/connector/MongoSyncConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sync.connector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSyncConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
