2e4837afd7ba6e10c2a703b558b39dd084e48858
jaeyounglee <ljyda214@gmail.com>
2025-09-17 22:44:51 +0900

    chg: consumer config 변경 및 임시로 configuration 에서 제외

--- src/main/java/org/sync/connector/consumer/KafkaConsumerConfig.java
+++ src/main/java/org/sync/connector/consumer/KafkaConsumerConfig.java
@@ -1,28 +1,34 @@
 package org.sync.connector.consumer;
 
-import java.beans.BeanProperty;
 import java.util.Map;
 
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+
+// @Configuration
 public class KafkaConsumerConfig {
 	@Bean
 	public ConsumerFactory<String, Object> consumerFactory() {
 		Map<String, Object> props = Map.of(
-				"bootstrap.servers", "localhost:9092",
-				"group.id", "example-group",
-				"key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer",
-				"value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"
+			"bootstrap.servers", "localhost:9092",
+			"group.id", "example-group",
+			"key.deserializer", StringSerializer.class.getName(),
+			"value.deserializer", KafkaAvroDeserializer.class.getName()
 		);
 
 		return new DefaultKafkaConsumerFactory<>(props);
 	}
 
 	@Bean
-	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(ConsumerFactory<String, Object > consumerFactory) {
+	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+		ConsumerFactory<String, Object> consumerFactory) {
 		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
 		factory.setConsumerFactory(consumerFactory);
 		return factory;
